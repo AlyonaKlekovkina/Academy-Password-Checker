5a65871f67713e46fb5041a51d92c6755ed8097f
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2024-12-09 11:08:24 +0100

    Add password length check and SHA-1 hashing functionality to game.py

--- game.py
+++ game.py
@@ -1,10 +1,19 @@
+import hashlib
+
 def length_check():
     while True:
         inp = input("Enter your password: ")
         if len(inp) < 8:
             print("Your password is too short. Please enter a password of at least 8 characters.")
         else:
-            return "You entered:  ", inp
+            return inp
 
-print(length_check())
 
+def create_sha1_hash(input_from_user):
+    result = hashlib.sha1(input_from_user.encode()).hexdigest()
+    return result
+
+
+checked_input = length_check()
+hashed_result = create_sha1_hash(checked_input)
+print('Your hashed password is: ', hashed_result)
