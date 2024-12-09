5cf1c2c28ecf1d66d267d33ca298c1f9de7fa9b5
AlyonaKlekovkina <like.the.sunshine.17@gmail.com>
2024-12-09 20:10:56 +0100

    Enhance password breach check functionality by adding regex parsing and response handling. Implement API request to verify if the password has been compromised and update output messages accordingly.

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import hashlib
 import requests
+import re
 
 
 def length_check():
@@ -18,15 +19,23 @@ def create_sha1_hash(input_from_user):
 def send_request(full_hashed_password):
     first_five_chars = full_hashed_password[:5]
     address = 'https://api.pwnedpasswords.com/range/{}'.format(first_five_chars)
-    #response = requests.get(address)
-    return address
+    response = requests.get(address)
+    last_chars = full_hashed_password[5:]
+    full_response = response.text
+    s = re.split(r"[~\r\n]+", full_response)
+    for i in s:
+        if last_chars in i.lower():
+            result = i.split(':')
+            return result[1]
 
 
 checked_input = length_check()
 hashed_result = create_sha1_hash(checked_input)
 print('Your hashed password is: ', hashed_result)
 print('Checking...')
-print('A request was sent to ')
-full_address = send_request(hashed_result)
-print('"{}"'.format(full_address))
-print('endpoint, awaiting response...')
+resp = send_request(hashed_result)
+message = 'Your password has been pwned! The password "{}" appears {} times in data breaches.'.format(checked_input, resp)
+if resp is None:
+    print("Good news! Your password hasn't been pwned.")
+else:
+    print(message)
